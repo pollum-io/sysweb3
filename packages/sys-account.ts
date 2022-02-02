9562b50112bdbf5684eb5abd3014549a024246cc
Leo Faleiros <leonardo@elint.com.br>
2022-02-02 13:42:28 -0300

    feat/implementing-web: Create web3Provider and import account with web3

--- packages/sys-account.ts
+++ packages/sys-account.ts
@@ -1,4 +1,4 @@
-import Web3 from 'web3';
+import web3Provider from '../src/web3Provider';
 
 interface IWeb3Account {
   address: string;
@@ -8,26 +8,13 @@ interface IWeb3Account {
   encrypt: Function;
 }
 
-const sysAccount = () => {
-  const urlProvider =
-    'https://api-rinkeby.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=3QSU7T49W5YYE248ZRF1CPKPRN7FPRPBKH';
+export const createAccount = () => {
+  const newAccount: IWeb3Account = web3Provider.eth.accounts.create();
 
-  const createAccount = () => {
-    const web3: Web3 = new Web3(new Web3.providers.HttpProvider(urlProvider));
-    const newAccount: IWeb3Account = web3.eth.accounts.create();
-
-    return {
-      address: newAccount?.address,
-      privateKey: newAccount?.privateKey,
-    };
-  };
   return {
-    createAccount,
+    address: newAccount?.address,
+    privateKey: newAccount?.privateKey,
   };
 };
 
-const { createAccount } = sysAccount();
-
 console.log(createAccount());
-
-export default sysAccount;
--- packages/sys-import.ts
+++ packages/sys-import.ts
@@ -1,9 +1,31 @@
-import Web3 from 'web3';
+import web3Provider from '../src/web3Provider';
 
-const sysImportAccount = () => {
-  return console.log('running');
+interface IWeb3ImportAccount {
+  address: string;
+  privateKey: string;
+  signTransaction: Function;
+  sign: Function;
+  encrypt: Function;
+}
+
+const sysImportAccount = (key: string) => {
+  const {
+    address,
+    privateKey,
+    signTransaction,
+    sign,
+    encrypt,
+  }: IWeb3ImportAccount = web3Provider.eth.accounts.privateKeyToAccount(
+    `${key}`
+  );
+
+  return { address, privateKey, signTransaction, sign, encrypt };
 };
 
-console.log(sysImportAccount());
+console.log(
+  sysImportAccount(
+    '0x8be8cdcf5a1b070d5ea7ed4e24d67424e3975ac45370cf015179274412f169ae'
+  )
+);
 
 export default sysImportAccount;
