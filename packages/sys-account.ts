e31f85bce62d9b0ffa9908d42cc56ac8323550af
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-02-02 15:43:18 -0300

    refactor: change code structure

--- packages/sys-account.ts
+++ packages/sys-account.ts
@@ -1,13 +1,3 @@
 import web3Provider from '../provider/web3Provider';
-import { IWeb3Account } from '../types/web3AccountType';
 
-export const createAccount = () => {
-  const newAccount: IWeb3Account = web3Provider.eth.accounts.create();
-
-  return {
-    address: newAccount?.address,
-    privateKey: newAccount?.privateKey,
-  };
-};
-
-console.log(createAccount());
+export const createAccount = () => web3Provider.eth.accounts.create();
--- packages/sys-import.ts
+++ packages/sys-import.ts
@@ -1,17 +1,3 @@
 import web3Provider from '../provider/web3Provider';
-import { IWeb3Account } from '../types/web3AccountType';
 
-const sysImportAccount = (key: string) => {
-  const { address, privateKey, signTransaction, sign, encrypt }: IWeb3Account =
-    web3Provider.eth.accounts.privateKeyToAccount(`${key}`);
-
-  return { address, privateKey, signTransaction, sign, encrypt };
-};
-
-console.log(
-  sysImportAccount(
-    '0x8be8cdcf5a1b070d5ea7ed4e24d67424e3975ac45370cf015179274412f169ae'
-  )
-);
-
-export default sysImportAccount;
+export const sysImportAccount = (privateKey: string) => web3Provider.eth.accounts.privateKeyToAccount(`${privateKey}`);
