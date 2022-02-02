46e124061bb628a40328be3f1567a9a30d23ce3a
Leo Faleiros <leonardo@elint.com.br>
2022-02-02 13:22:52 -0300

    feat/implementing-web3: Start the web3 implementation and create the sys-account.ts to test the account creation

--- packages/hello.ts
+++
@@ -1,3 +0,0 @@
-const hello = () => console.log('hello from syscoin web3');
-
-export default hello;
---
+++ packages/sys-import.ts
@@ -0,0 +1,9 @@
+import Web3 from 'web3';
+
+const sysImportAccount = () => {
+  return console.log('running');
+};
+
+console.log(sysImportAccount());
+
+export default sysImportAccount;
