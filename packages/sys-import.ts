54ecc2d2bda2c8c1db86a4fe68e05b36bb278f52
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-02-03 14:59:49 -0300

    fix: remove types

--- packages/sys-import.ts
+++ packages/sys-import.ts
@@ -1,3 +1,3 @@
 import web3Provider from '../provider/web3Provider';
 
-export const sysImportAccount = (privateKey: string) => web3Provider.eth.accounts.privateKeyToAccount(privateKey);
+export const sysImportAccount = (privateKey) => web3Provider.eth.accounts.privateKeyToAccount(privateKey);
