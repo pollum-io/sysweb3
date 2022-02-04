b48bb7e472a97a9afc5c873a49468a4826828285
Leo Faleiros <leonardo@elint.com.br>
2022-02-04 13:30:39 -0300

    feat/implementing-web3: Finish getBalance with new Provider

--- packages/sys-balance.ts
+++ packages/sys-balance.ts
@@ -2,12 +2,10 @@ import web3Provider from '../provider/web3Provider';
 
 export const sysGetBalance = async (walletAddress: string) => {
   try {
-    await web3Provider.eth
-      .getBalance(walletAddress)
-      .then((balance) => console.log(balance));
+    const balance = await web3Provider.eth.getBalance(walletAddress);
+
+    return web3Provider.utils.toWei(balance, 'ether');
   } catch (error) {
     console.log(`${error}`);
   }
 };
-
-console.log(sysGetBalance('0x8B06aFc57FdC8C33834A0bcf81a5326f8d46e8E5'));
