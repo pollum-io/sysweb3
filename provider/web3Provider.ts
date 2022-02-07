95e53aafb4177a336eb63a41fcbdfb67d8c6c2e6
Leo Faleiros <leonardo@elint.com.br>
2022-02-07 16:11:31 -0300

    feat/implementing-web3: Create new networks and create a function to receive a chainID and return it provider

--- provider/web3Provider.ts
+++ provider/web3Provider.ts
@@ -1,8 +1,24 @@
 import Web3 from 'web3';
+import { networks } from '../networks/networks';
 
-const urlProvider =
-  'https://mainnet.infura.io/v3/c42232a29f9d4bd89d53313eb16ec241';
+const web3Provider = (chainId) => {
+  let provider;
 
-const web3Provider = new Web3(new Web3.providers.HttpProvider(urlProvider));
+  if (chainId) {
+    networks.map((net) => {
+      if (net.chainId === chainId) {
+        provider = new Web3(new Web3.providers.HttpProvider(net.url));
+      }
+    });
+  } else {
+    new Web3(
+      new Web3.providers.HttpProvider(
+        'https://mainnet.infura.io/v3/c42232a29f9d4bd89d53313eb16ec241'
+      )
+    );
+  }
+
+  return provider;
+};
 
 export default web3Provider;
