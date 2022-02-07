4db287985ac8c1631226601140ad0765c681e864
Leo Faleiros <leonardo@elint.com.br>
2022-02-07 17:56:44 -0300

    feat/implementing-web3: Put the changeNetwork and Provider functions in the same file to don't need state anymore

--- provider/web3Provider.ts
+++ provider/web3Provider.ts
@@ -1,16 +1,28 @@
 import Web3 from 'web3';
+import { networks } from '../networks/networks';
 
-const web3Provider = (url?: string) => {
-  let currentUrl = url;
+let provider: any;
 
-  let defaultUrl =
-    'https://mainnet.infura.io/v3/c42232a29f9d4bd89d53313eb16ec241';
+export const changeNetwork = (chainId: number) => {
+  if (chainId) {
+    networks.map((net) => {
+      if (net.chainId === chainId) {
+        provider = net.url;
+      }
+    });
+  }
+};
 
-  console.log(currentUrl);
-
+export const web3Provider = () => {
   return new Web3(
-    new Web3.providers.HttpProvider(currentUrl ? currentUrl : defaultUrl)
+    new Web3.providers.HttpProvider(
+      provider ||
+        'https://mainnet.infura.io/v3/c42232a29f9d4bd89d53313eb16ec241'
+    )
   );
 };
+console.log(changeNetwork(1));
+
+console.log(web3Provider().eth.currentProvider);
 
 export default web3Provider;
