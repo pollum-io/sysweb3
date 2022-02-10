bdae48b0d9c50aa5c9b64250878da5e4bb3364eb
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-02-10 11:43:30 -0300

    feat: create method to import sys account with seed phrase

--- packages/web3-import.ts
+++ packages/web3-import.ts
@@ -1,4 +1,11 @@
 import { web3Provider } from '../provider/web3Provider';
+import { ethers } from 'ethers';
 
-export const sysImportAccount = (privateKey) =>
-  web3Provider().eth.accounts.privateKeyToAccount(privateKey);
+
+export const sysImportAccount = (mnemonic: string) => {
+  const sysMnemonicAccount = ethers.Wallet.fromMnemonic(mnemonic);
+  
+  const sysToWeb3 = web3Provider().eth.accounts.privateKeyToAccount(sysMnemonicAccount.privateKey);
+
+  return sysToWeb3;
+};
