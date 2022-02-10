0fd76e456a75149f0024bb69b1ac4313036a03be
Leo Faleiros <leonardo@elint.com.br>
2022-02-10 15:21:52 -0300

    feat/implementing-web3: Fix the importAccount function names

--- packages/web3-import.ts
+++ packages/web3-import.ts
@@ -1,11 +1,12 @@
 import { web3Provider } from '../provider/web3Provider';
 import { ethers } from 'ethers';
 
-
 export const importAccount = (mnemonic: string) => {
   const mnemonicAccount = ethers.Wallet.fromMnemonic(mnemonic);
-  
-  const web3MnemonicAccount = web3Provider().eth.accounts.privateKeyToAccount(sysMnemonicAccount.privateKey);
+
+  const web3MnemonicAccount = web3Provider().eth.accounts.privateKeyToAccount(
+    mnemonicAccount.privateKey
+  );
 
   return web3MnemonicAccount;
 };
