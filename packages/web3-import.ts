ec211b68938aa419a1b3faa80c3d75fffbc0b4c7
Leo Faleiros <leonardo@elint.com.br>
2022-02-10 16:10:42 -0300

    feat/implementing-web3: Implement a verification to know if users give a private key or a mnemonic to the import account function

--- packages/web3-import.ts
+++ packages/web3-import.ts
@@ -1,12 +1,18 @@
 import { web3Provider } from '../provider/web3Provider';
 import { ethers } from 'ethers';
 
-export const importAccount = (mnemonic: string) => {
-  const mnemonicAccount = ethers.Wallet.fromMnemonic(mnemonic);
+export const importAccount = (mnemonicOrPrivateKey: string) => {
+  if (web3Provider().utils.isHexStrict(mnemonicOrPrivateKey)) {
+    return web3Provider().eth.accounts.privateKeyToAccount(
+      mnemonicOrPrivateKey
+    );
+  } else {
+    const mnemonicAccount = ethers.Wallet.fromMnemonic(mnemonicOrPrivateKey);
 
-  const web3MnemonicAccount = web3Provider().eth.accounts.privateKeyToAccount(
-    mnemonicAccount.privateKey
-  );
+    const web3MnemonicAccount = web3Provider().eth.accounts.privateKeyToAccount(
+      mnemonicAccount.privateKey
+    );
 
-  return web3MnemonicAccount;
+    return web3MnemonicAccount;
+  }
 };
