334df86a9258d2317e65bfaba87c9f25d946363e
Lucas Gauer <lucasfgauer97@gmail.com>
2022-02-16 12:14:38 -0300

    test: Add changeNetwork unit test~

---
+++ provider/web3Provider.spec.ts
@@ -0,0 +1,19 @@
+import Web3 from 'web3';
+import { changeNetwork, web3Provider } from './web3Provider';
+
+describe('web3Provider', () => {
+
+  //* changeNetwork
+  it('should change the network', async () => {
+    // 5700 = testnet chainId
+    await changeNetwork(5700);
+
+    const provider = web3Provider().currentProvider;
+    const { HttpProvider } = Web3.providers;
+    expect(provider).toBeInstanceOf(HttpProvider);
+
+    if (!(provider instanceof HttpProvider)) return;
+    expect(provider.host).toBe('https://rpc.tanenbaum.io/')
+  });
+
+});
