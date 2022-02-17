0945b609de1b0e0fd70654de32dc44c057558fa2
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-02-17 11:10:13 -0300

    refactor: change importAccount structure

--- packages/web3-import.ts
+++ packages/web3-import.ts
@@ -1,23 +1,28 @@
 import { web3Provider } from '../provider/web3Provider';
 import { ethers } from 'ethers';
 import CryptoJS from 'crypto-js';
+import { mnemonicToSeed } from 'bip39';
+import { hdkey } from 'ethereumjs-wallet';
 
-export const importAccount = (mnemonicOrPrivateKey, pwdAccount) => {
+export const importAccount = async (mnemonicOrPrivateKey, pwdAccount) => {
   if (web3Provider().utils.isHexStrict(mnemonicOrPrivateKey)) {
     return web3Provider().eth.accounts.privateKeyToAccount(
       mnemonicOrPrivateKey
     );
   } else {
-    const descryptMnemonic = CryptoJS.AES.decrypt(
+    const decryptMnemonic = CryptoJS.AES.decrypt(
       mnemonicOrPrivateKey,
       pwdAccount
     ).toString(CryptoJS.enc.Utf8);
 
-    const mnemonicAccount = ethers.Wallet.fromMnemonic(descryptMnemonic);
+    const info = await mnemonicToSeed(decryptMnemonic);
+    const hdwallet = hdkey.fromMasterSeed(info);
+    const wallet_hdpath = "m/44'/60'/0'/0/";
+    const wallet = hdwallet.derivePath(wallet_hdpath + 1).getWallet();
+    const privateKey = '0x' + wallet.getPrivateKey().toString('hex');
 
-    const web3MnemonicAccount = web3Provider().eth.accounts.privateKeyToAccount(
-      mnemonicAccount.privateKey
-    );
+    const web3MnemonicAccount =
+      await web3Provider().eth.accounts.privateKeyToAccount(privateKey);
 
     return web3MnemonicAccount;
   }
