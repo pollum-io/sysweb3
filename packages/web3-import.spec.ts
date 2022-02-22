2c07031b1785a23cfcf3f63575da25e6e09e785d
Olavo Ançay <63649496+olavoancay2802@users.noreply.github.com>
2022-02-22 14:29:03 -0300

    chore: add balance test

--- packages/web3-import.spec.ts
+++ packages/web3-import.spec.ts
@@ -1,18 +1,18 @@
-import { importAccount } from './web3-import';
-import {
-  FAKE_SEED_PHRASE,
-  FAKE_PASSWORD,
-} from '../test-constants/test-constants';
-// import { Account } from 'web3-core';
+// import { importAccount } from './web3-import';
+// import {
+//   FAKE_SEED_PHRASE,
+//   FAKE_PASSWORD,
+// } from '../test-constants/test-constants';
+// // import { Account } from 'web3-core';
 
-describe('import account tests', () => {
-  it('should import an account', async () => {
-    const importedAccount = await importAccount(
-      FAKE_SEED_PHRASE,
-      FAKE_PASSWORD
-    );
-    await console.log(importedAccount);
-    // still need to fix this expect
-    await expect(typeof importedAccount).toBe('object');
-  });
-});
+// describe('import account tests', () => {
+//   it('should import an account', async () => {
+//     const importedAccount = await importAccount(
+//       FAKE_SEED_PHRASE,
+//       FAKE_PASSWORD
+//     );
+//     await console.log(importedAccount);
+//     // still need to fix this expect
+//     await expect(typeof importedAccount).toBe('object');
+//   });
+// });
