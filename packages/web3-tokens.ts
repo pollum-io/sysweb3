99130c62f9e0f3f57c54ed5c37ac51054e9ae30c
Leonardo Faleiros <leonardo.faleiros@pollum.io>
2022-03-04 15:52:53 -0300

    feat/implementing-web3: Create a function to return the token icon url using the token symbol to search at coingecko

--- packages/web3-tokens.ts
+++ packages/web3-tokens.ts
@@ -55,62 +55,9 @@ export const getTokens = async (walletAddress: string) => {
     });
 
     if (tokens.ethereum.address[0].balances) {
-      const walletTokens = tokens.ethereum.address[0].balances;
+      const tokensInUserWallet: any[] = tokens.ethereum.address[0].balances;
 
-      let finalTokens: any;
-
-      walletTokens.map(async (token: any) => {
-        const searchForToken = await axios.get(
-          `https://api.coingecko.com/api/v3/search?query=${token.currency.symbol}`
-        );
-
-        const searchForEachToken = searchForToken.data.coins.filter(
-          (coinGeckoToken: any) =>
-            coinGeckoToken.symbol === token.currency.symbol
-        );
-
-        for (let i = 0; i < searchForEachToken.length; i++) {
-          const object = {
-            symbol: token.currency.symbol,
-            value: token.value,
-            image: searchForEachToken[i].thumb,
-          };
-
-          finalTokens.push(object);
-
-          // return finalTokens;
-          // console.log('final', finalTokens);
-        }
-      });
-
-      console.log('final', finalTokens);
-
-      // for (let i = 0; i < walletTokens.length; i++) {
-      //   const searchForToken = await axios.get(
-      //     `https://api.coingecko.com/api/v3/search?query=${walletTokens[i].currency.symbol}`
-      //   );
-
-      //   console.log('searchForToken', searchForToken.data.coins);
-
-      //   const searchForEachToken = searchForToken.data.coins.filter(
-      //     (token: any) => token.symbol === walletTokens[i].currency.symbol
-      //   );
-
-      //   console.log('searchForEachToken', searchForEachToken);
-
-      //   const returnTokens = {
-      //     symbol: walletTokens[i].currency.symbol,
-      //     value: walletTokens[i].value,
-      //     // thumbImage: searchForEachToken[0].thumb,
-      //     // largeImage: searchForEachToken[0].large,
-      //   };
-
-      //   console.log('RETURNNNNN', returnTokens);
-
-      //   // return returnTokens;
-      // }
-
-      // return tokens.ethereum.address[0].balances;
+      return tokensInUserWallet;
     } else {
       throw new Error('Not available tokens');
     }
@@ -119,4 +66,18 @@ export const getTokens = async (walletAddress: string) => {
   }
 };
 
-getTokens('0xb1Ef4840213e387e5Cebcf5472d88fE9C2775dFa');
+const getTokenIconBySymbol = async (symbol: string) => {
+  const generalSearchForToken = await axios.get(
+    `https://api.coingecko.com/api/v3/search?query=${symbol}`
+  );
+
+  const getSpecificToken = generalSearchForToken.data.coins.filter(
+    (token: any) => {
+      return token.symbol === symbol;
+    }
+  );
+
+  const { thumb, large } = getSpecificToken[0];
+
+  return { thumbImage: thumb, largeImage: large };
+};
