17897dfbe95c0452407786f0cdc8e4c932398b8a
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-03-05 18:13:18 -0300

    feat: add javascript files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-export const { createAccount } = require("./web3-account");
-export const { getBalance } = require("./web3-balance");
-export const { importAccount } = require('./web3-import')
-export const { getNFTImage, getUserNFT } = require('./web3-nft');
-export const { getTokenIconBySymbol, getTokens } = require('./web3-tokens');
-export const { sendTransactions } = require('./web3-transactions')
-export const { changeNetwork } = require('./web3Provider');
