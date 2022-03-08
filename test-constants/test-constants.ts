e7b8c562a4f6d2811a73b73453fadc3ecb18f5f1
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-08 13:18:41 -0300

    test: Update test data: add private key, update address

--- test-constants/test-constants.ts
+++ test-constants/test-constants.ts
@@ -4,4 +4,5 @@ export const FAKE_SEED_PHRASE =
   'peace uncle grit essence stuff angle cruise annual fury letter snack globe';
 export const INVALID_SEED_PHRASE =
   'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor';
-export const FAKE_ADDRESS = '0x0beadde9e116cef07afedc45a8566d1add3168f3';
+export const FAKE_ADDRESS = '0x4d4DB937177Ceb77aF4541b4EC9Ae8b0EA5d1a64';
+export const FAKE_PRIV_KEY = '0x27cf026f5657ad3403b767f16e0c46eeaa03fe0ff1903ad5a84d448263255a2b';
