c22549c9897ab29da83a09dc53f1316913c17196
Olavo Ançay <63649496+olavoancay2802@users.noreply.github.com>
2022-03-10 09:50:25 -0300

    test: add getNFTImage test

--- packages/web3-nft.spec.ts
+++ packages/web3-nft.spec.ts
@@ -1,5 +1,5 @@
+import { changeNetwork } from '../provider/web3Provider';
 import { getUserNFT, getNFTImage } from './web3-nft';
-import { FAKE_ADDRESS } from '../test-constants/test-constants';
 
 describe('web3-NFT tests', () => {
   it('should get user NFTs', async () => {
@@ -10,5 +10,15 @@ describe('web3-NFT tests', () => {
       const blocknumber = userNFT[0].blockNumber;
       expect(blocknumber.length > 0).toBe(true);
     }
+  });
+
+  it('should check NFT url', async () => {
+    changeNetwork(1);
+    const NFTUrl = await getNFTImage(
+      '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+      8520
+    );
+
+    expect(NFTUrl.startsWith('https://ipfs.io/ipfs/')).toBe(true);
   });
 });
