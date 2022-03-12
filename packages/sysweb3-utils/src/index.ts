10c0a53d67fcbc775841cc2d3650e7d57930bfef
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-03-12 01:06:28 -0300

    feat: create function to validate rpc provider

--- packages/sysweb3-utils/src/index.ts
+++ packages/sysweb3-utils/src/index.ts
@@ -2,3 +2,4 @@ export * from "./contractInstance";
 export * from "./web3GetNFTImage";
 export * from "./web3GetTokenIcon";
 export * from "./web3ValidateToken";
+export * from "./web3ValidateRPC";
---
+++ packages/sysweb3-utils/src/web3ValidateRPC.ts
@@ -0,0 +1,17 @@
+import { web3Provider } from "../../sysweb3-networks/index";
+
+
+export const RpcVerifier = () => {
+    return web3Provider.eth.net.isListening((err, res) => {
+      if (err) {
+        return console.error({
+          message: 'Check the current RPC. Maybe is not a valid RPC.',
+          valid: res === undefined ? false : null,
+        });
+      }
+      return console.log({
+        message: 'The current RPC is working correctly.',
+        valid: res,
+      });
+    });
+  };
