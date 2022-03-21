d75814972e10b17437b04fab282564168a709baf
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-21 14:17:21 -0300

    test: Move  test to new folder structure

--- packages/sysweb3-utils/test/tokens.spec.ts
+++ packages/sysweb3-utils/test/tokens.spec.ts
@@ -1,10 +1,10 @@
-import { changeNetwork } from '../provider/web3Provider';
-import { getUserNFT, getNFTImage } from './web3-nft';
+// import { changeNetwork } from '../provider/web3Provider';
+import { getNftImage } from '../src/tokens';
 
 describe('web3-NFT tests', () => {
   it('should check NFT url', async () => {
-    changeNetwork(1);
-    const NFTUrl = await getNFTImage(
+    // changeNetwork(1);
+    const NFTUrl = await getNftImage(
       '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
       8520
     );
