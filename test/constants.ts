4cee6ec1dceddc79db18f6dbcca405f1eff87c13
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-21 14:09:13 -0300

    chore: Move test constants

--- test/constants.ts
+++ test/constants.ts
@@ -5,4 +5,5 @@ export const FAKE_SEED_PHRASE =
 export const INVALID_SEED_PHRASE =
   'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor';
 export const FAKE_ADDRESS = '0x4d4DB937177Ceb77aF4541b4EC9Ae8b0EA5d1a64';
-export const FAKE_PRIV_KEY = '0x27cf026f5657ad3403b767f16e0c46eeaa03fe0ff1903ad5a84d448263255a2b';
+export const FAKE_PRIV_KEY =
+  '0x27cf026f5657ad3403b767f16e0c46eeaa03fe0ff1903ad5a84d448263255a2b';
