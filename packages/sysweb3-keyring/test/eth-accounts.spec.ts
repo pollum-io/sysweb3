fc7b83b8e815afb2c87cee4d2561d0bc8370673b
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-21 14:09:56 -0300

    test: Move  test to new folder structure

--- packages/sysweb3-keyring/test/eth-accounts.spec.ts
+++ packages/sysweb3-keyring/test/eth-accounts.spec.ts
@@ -1,12 +1,18 @@
+import { FAKE_ADDRESS } from '../../../test/constants';
 import { Web3Accounts } from '../src/accounts/eth-accounts';
 
-const { createAccount } = Web3Accounts();
-
 describe('Web3Accounts', () => {
+  const { getBalance, createAccount } = Web3Accounts();
+
   it('should create an account', () => {
     const newAccount = createAccount();
 
     expect(newAccount).toBeTruthy();
     expect(newAccount.address).toBeTruthy();
+  });
+
+  it('should get balance', async () => {
+    const balance = await getBalance(FAKE_ADDRESS);
+    expect(typeof balance).toBe('number');
   });
 });
