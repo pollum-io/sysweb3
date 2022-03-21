bd45138adfa8eb42d65a0fae6b5d3c4e04bd649b
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-21 14:26:44 -0300

    test: Move `sendTransaction` test to new folder structure

--- packages/sysweb3-keyring/test/eth-accounts.spec.ts
+++ packages/sysweb3-keyring/test/eth-accounts.spec.ts
@@ -13,6 +13,8 @@ describe('Web3Accounts', () => {
     importAccount,
     getNftsByAddress,
     getTokens,
+    sendTransaction,
+    setActiveNetwork,
   } = Web3Accounts();
 
   it('should create an account', () => {
@@ -65,5 +67,22 @@ describe('Web3Accounts', () => {
       const firstTokenValue = tokens[0].value;
       expect(typeof firstTokenValue).toBe('number');
     }
+  });
+
+  jest.setTimeout(15000);
+
+  it('should send a transaction', async () => {
+    // change to Rinkeby network
+    setActiveNetwork(4);
+    const transaction = await sendTransaction(
+      // test web3 account
+      '0x6e578c2227bc4629794e566610209c9cb7a35341f13de4ba886a59a4e11b7d1e',
+      // Pali web3 account
+      '0x6a92eF94F6Db88098625a30396e0fde7255E97d5',
+      0.01
+    );
+
+    const blockNumber = transaction.blockNumber;
+    expect(typeof blockNumber).toBe('number');
   });
 });
