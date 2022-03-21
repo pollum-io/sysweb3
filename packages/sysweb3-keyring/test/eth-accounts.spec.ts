56289f66e32f17464a1c6b3925f9e92a75e2fa1b
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-21 14:11:50 -0300

    test: Move  tests to new folder structure

--- packages/sysweb3-keyring/test/eth-accounts.spec.ts
+++ packages/sysweb3-keyring/test/eth-accounts.spec.ts
@@ -1,8 +1,13 @@
-import { FAKE_ADDRESS } from '../../../test/constants';
+import {
+  FAKE_ADDRESS,
+  FAKE_PASSWORD,
+  FAKE_PRIV_KEY,
+  FAKE_SEED_PHRASE,
+} from '../../../test/constants';
 import { Web3Accounts } from '../src/accounts/eth-accounts';
 
 describe('Web3Accounts', () => {
-  const { getBalance, createAccount } = Web3Accounts();
+  const { getBalance, createAccount, importAccount } = Web3Accounts();
 
   it('should create an account', () => {
     const newAccount = createAccount();
@@ -14,5 +19,25 @@ describe('Web3Accounts', () => {
   it('should get balance', async () => {
     const balance = await getBalance(FAKE_ADDRESS);
     expect(typeof balance).toBe('number');
+  });
+
+  it('should import an account using a private key', async () => {
+    const importedAccount = importAccount(FAKE_PRIV_KEY, '');
+
+    expect(importedAccount).toBeTruthy();
+    expect(importedAccount.address).toEqual(FAKE_ADDRESS);
+  });
+
+  it('should import an account using a seed phrase (mnemonic)', async () => {
+    //* encrypt the mnemonic
+    const encryptedMnemonic = CryptoJS.AES.encrypt(
+      FAKE_SEED_PHRASE,
+      FAKE_PASSWORD
+    ).toString();
+
+    const importedAccount = importAccount(encryptedMnemonic, FAKE_PASSWORD);
+
+    expect(importedAccount).toBeTruthy();
+    expect(importedAccount.address).toBeTruthy();
   });
 });
