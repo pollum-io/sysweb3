0176752c49222ac4375a6e9e9733c86f7a3ea0f7
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-21 14:10:12 -0300

    test: Move  test to new folder structure

--- packages/web3-balance.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getBalance } from './web3-balance';
-import { FAKE_ADDRESS } from '../test-constants/test-constants';
-
-describe('Balance tests', () => {
-  it('should get balance', async () => {
-    const balance = await getBalance(FAKE_ADDRESS);
-    expect(typeof balance).toBe('number');
-  });
-});
