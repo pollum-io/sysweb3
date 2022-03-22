1ab0afcbdb5858687a2e816dafe2cbb80e824036
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-22 17:41:44 -0300

    chore: Move `setActiveNetwork` to network

--- packages/sysweb3-network/src/providers/web3.ts
+++ packages/sysweb3-network/src/providers/web3.ts
@@ -1,5 +1,27 @@
 import Web3 from 'web3';
+import { networks } from '../networks';
 
 export const web3Provider = new Web3(
   new Web3.providers.HttpProvider('https://rpc.syscoin.org/')
 );
+
+/**
+ *
+ * Available networks:
+ * - Syscoin Mainnet (57) and Testnet (5700)
+ * - Ethereum Mainnet (1)
+ * - Ethereum Rinkeby (4)
+ * - Polygon Mainnet (137) and Testnet (80001)
+ *
+ * @param chainId chain id of the network to set as active
+ * @returns void
+ *
+ */
+export const setActiveNetwork = (chainId: number): void => {
+  const network = networks[chainId];
+
+  if (!network) throw new Error('Network not found');
+
+  const { HttpProvider } = Web3.providers;
+  web3Provider.setProvider(new HttpProvider(network.url));
+};
