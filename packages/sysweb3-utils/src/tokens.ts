1bad3f5def7f6a5d1529000e9c7e791f1b6cd442
Leonardo Faleiros <leonardo.faleiros@pollum.io>
2022-03-23 17:00:14 -0300

    Add new function to sysweb3-utils/tokens where we can get the Token price with any fiat using the token name and the current fiat chosen

--- packages/sysweb3-utils/src/tokens.ts
+++ packages/sysweb3-utils/src/tokens.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import abi from './abi/erc721.json';
-import { createContractUsingAbi } from './index';
+import axios from "axios";
+import abi from "./abi/erc721.json";
+import { createContractUsingAbi } from "./index";
 
 export const getNftImage = async (contract: string, tokenId: number) => {
   try {
@@ -9,7 +9,7 @@ export const getNftImage = async (contract: string, tokenId: number) => {
       .call();
 
     if (nft) {
-      const ipfsUrl = String(nft).replace('ipfs://', 'https://ipfs.io/ipfs/');
+      const ipfsUrl = String(nft).replace("ipfs://", "https://ipfs.io/ipfs/");
 
       const url = await axios.get(ipfsUrl);
 
@@ -17,13 +17,13 @@ export const getNftImage = async (contract: string, tokenId: number) => {
        * 'https://gateway.pinata.cloud/ipfs/Qmc4DqK9xeoSvtVmTcS6YG3DiWHyfiwQsnwQfzcqAvtmHj'
        */
 
-      return String(url.data.image).replace('ipfs://', 'https://ipfs.io/ipfs/');
+      return String(url.data.image).replace("ipfs://", "https://ipfs.io/ipfs/");
     }
 
-    throw new Error('NFTinfo not found.');
+    throw new Error("NFTinfo not found.");
   } catch (error) {
     console.log(
-      'Verify current network. Set the same network of NFT contract.'
+      "Verify current network. Set the same network of NFT contract."
     );
     throw error;
   }
@@ -50,7 +50,7 @@ export const getTokenIconBySymbol = async (symbol: string) => {
     }
   } catch (error) {
     // todo: handle
-    throw new Error('Token icon not found');
+    throw new Error("Token icon not found");
   }
 };
 
@@ -61,9 +61,33 @@ export const isNFT = (guid: number) => {
 };
 
 export const getHost = (url: string) => {
-  if (typeof url === 'string' && url !== '') {
+  if (typeof url === "string" && url !== "") {
     return new URL(url).host;
   }
 
   return url;
+};
+
+export const getTokenPrice = async (tokenName: string, fiat: string) => {
+  try {
+    const tokenRequest = await axios
+      .get(`https://api.coingecko.com/api/v3/coins/${tokenName}`)
+      .then((response: any) => {
+        const { price_change_24h, current_price } = response.data.market_data;
+
+        const fiatPrice = current_price[fiat];
+
+        return {
+          current_price: fiatPrice,
+          current_price_change_24h: price_change_24h,
+        };
+      });
+
+    return {
+      token_price: tokenRequest.current_price,
+      price_change: tokenRequest.current_price_change_24h,
+    };
+  } catch (error) {
+    return 0;
+  }
 };
