36768af88a55fabd9f2ff90165fa3ab8e2d89664
Lucas Gauer <lucasfgauer97@gmail.com>
2022-03-24 16:26:35 -0300

    test: Create unit test for `getFiatValueByToken`

--- packages/sysweb3-utils/test/tokens.spec.ts
+++ packages/sysweb3-utils/test/tokens.spec.ts
@@ -1,5 +1,9 @@
 // import { changeNetwork } from '../provider/web3Provider';
-import { getNftImage, getTokenIconBySymbol } from '../src/tokens';
+import {
+  getFiatValueByToken,
+  getNftImage,
+  getTokenIconBySymbol,
+} from '../src/tokens';
 
 describe('web3-NFT tests', () => {
   it('should check NFT url', async () => {
@@ -15,5 +19,16 @@ describe('web3-NFT tests', () => {
   it('should get token icon by symbol', async () => {
     const token = await getTokenIconBySymbol('eth');
     expect(token.startsWith('https://')).toBe(true);
+  });
+
+  it('should retrive a token price as fiat', async () => {
+    const result = await getFiatValueByToken('syscoin', 'usd');
+    console.log(result);
+
+    expect(typeof result.price).toBe('number');
+    expect(result.price).toBeGreaterThan(0);
+
+    expect(typeof result.priceChange).toBe('number');
+    expect(result.priceChange).toBeGreaterThanOrEqual(0);
   });
 });
