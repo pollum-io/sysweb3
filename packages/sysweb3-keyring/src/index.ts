5e94e4054e34d8eaafeffabfade5b4fb5eadb162
Amanda Gonsalves <amandagonsalves75@gmail.com>
2022-03-28 02:45:02 -0300

    chore: wip

--- packages/sysweb3-keyring/src/index.ts
+++ packages/sysweb3-keyring/src/index.ts
@@ -2,3 +2,5 @@ export * from './keyring-manager';
 export * from './accounts';
 export * from './wallets/main';
 export * from './signer';
+export * from './transactions';
+export * from './trezor';
