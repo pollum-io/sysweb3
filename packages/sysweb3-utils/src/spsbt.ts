865278acaca2e79524365e0133ce74bc03bf89ee
Amanda Gonsalves <amandagonsalves75@gmail.com>
2022-03-29 20:09:00 -0300

    fix: types issues

---
+++ packages/sysweb3-utils/src/spsbt.ts
@@ -0,0 +1,25 @@
+import { Transaction, Psbt } from 'bitcoinjs-lib';
+
+export type Bip32 = {
+  public: number;
+  private: number;
+}
+
+export type BitcoinNetwork = {
+  messagePrefix: string;
+  bech32: string;
+  bip32: Bip32;
+  pubKeyHash: number;
+  scriptHash: number;
+  wif: number;
+}
+
+export interface SPSBT extends Psbt {
+  getFeeRate: () => number;
+  getFee: () => number;
+  extractTransaction: (disableFeeCheck?: boolean) => Transaction;
+  fromBase64: (data: string, options: {
+    network?: BitcoinNetwork;
+    maximumFeeRate?: number;
+  }) => Psbt;
+}
