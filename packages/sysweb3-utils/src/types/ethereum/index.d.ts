3fa95ceb442da5e8e849a55e5dac2eb3ab27dad5
Leonardo Faleiros <leonardo.faleiros@pollum.io>
2022-03-29 13:21:59 -0300

    Change the sendTransaction function to a new one using the custom gasFee parameter

--- packages/sysweb3-utils/src/types/ethereum/index.d.ts
+++ packages/sysweb3-utils/src/types/ethereum/index.d.ts
@@ -1,25 +1,37 @@
-import { Account, TransactionReceipt } from 'web3-core';
-import { IEthereumToken, IEthereumNft } from '.';
-export * from './tokens';
-export * from './transactions';
-export * from './accounts';
+import { Account, TransactionReceipt } from "web3-core";
+import { IEthereumToken, IEthereumNft } from ".";
+export * from "./tokens";
+export * from "./transactions";
+export * from "./accounts";
 export declare type web3 = {
-    createAccount: () => Account;
-    getBalance: (address: string) => Promise<string>;
-    importAccount: (mnemonic: string, password: string) => Account;
-    sendTransaction: (sender: string, receiver: string, value: number) => Promise<TransactionReceipt>;
-    setActiveNetwork: (chainId: number) => void;
-    getNFTImage: (address: string, tokenId: number) => Promise<string | void>;
-    getNftsByAddress: (address: string) => Promise<IEthereumNft[]>;
-    getTokens: (address: string, tokenAddress: string) => Promise<IEthereumToken | undefined>;
-    getTokenIconBySymbol: (symbol: string) => Promise<{
+  createAccount: () => Account;
+  getBalance: (address: string) => Promise<string>;
+  importAccount: (mnemonic: string, password: string) => Account;
+  sendTransaction: (
+    fromAddress: string,
+    fromPrivateKey: string,
+    toAddress: string,
+    value: number,
+    gasFee?: string
+  ) => Promise<TransactionReceipt>;
+  setActiveNetwork: (chainId: number) => void;
+  getNFTImage: (address: string, tokenId: number) => Promise<string | void>;
+  getNftsByAddress: (address: string) => Promise<IEthereumNft[]>;
+  getTokens: (
+    address: string,
+    tokenAddress: string
+  ) => Promise<IEthereumToken | undefined>;
+  getTokenIconBySymbol: (symbol: string) => Promise<
+    | {
         thumbImage: string;
         largeImage: string;
-    } | undefined>;
-    web3Provider: any;
-    validateCurrentRpc: () => Promise<boolean>;
+      }
+    | undefined
+  >;
+  web3Provider: any;
+  validateCurrentRpc: () => Promise<boolean>;
 };
 export declare enum ISysWeb3 {
-    web3 = 0,
-    syscoin = 1
+  web3 = 0,
+  syscoin = 1,
 }
