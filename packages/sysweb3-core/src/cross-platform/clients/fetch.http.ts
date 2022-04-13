41ff325ad3ccd2f4984f60844a793cfbfe0d3796
Lucas Gauer <lucasfgauer97@gmail.com>
2022-04-13 11:48:13 -0300

    fix: Update 'default' assing to avoid 'window is not defined' error

--- packages/sysweb3-core/src/cross-platform/clients/fetch.http.ts
+++ packages/sysweb3-core/src/cross-platform/clients/fetch.http.ts
@@ -1,4 +1,6 @@
-const defaultFetch = window.fetch.bind(window) ? window.fetch.bind(window) : undefined;
+declare let window: any;
+const defaultFetch =
+  typeof window !== 'undefined' ? window.fetch.bind(window) : undefined;
 
 export const FetchRestService = (httpClient: any = defaultFetch) => {
   const invoke = (options: RestApiOptionsRequest) => {
