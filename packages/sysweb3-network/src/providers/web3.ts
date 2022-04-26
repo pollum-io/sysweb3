c592528314e464702ce8e61f31f122befbc454fa
Amanda Gonsalves <amandagonsalvesags@gmail.com>
2022-04-26 16:10:46 -0300

    fix: build issue

--- packages/sysweb3-network/src/providers/web3.ts
+++ packages/sysweb3-network/src/providers/web3.ts
@@ -6,18 +6,6 @@ export const web3Provider = new Web3(
   new Web3.providers.HttpProvider('https://rpc.syscoin.org/')
 );
 
-/**
- *
- * Available networks:
- * - Syscoin Mainnet (57) and Testnet (5700)
- * - Ethereum Mainnet (1)
- * - Ethereum Rinkeby (4)
- * - Polygon Mainnet (137) and Testnet (80001)
- *
- * @param chainId chain id of the network to set as active
- * @returns void
- *
- */
 export const setActiveNetwork = (network: INetwork): void => {
   const { HttpProvider } = Web3.providers;
 
