fa5278b8b9fe759ce6fce028fd1f5a22ab2f9e83
Amanda Gonsalves <amandagonsalvesags@gmail.com>
2022-12-20 19:28:09 -0300

    test: sysweb3 network and rpcs tests

---
+++ packages/sysweb3-network/test/rpc.spec.ts
@@ -0,0 +1,54 @@
+import {
+  getBip44Chain,
+  isValidChainIdForEthNetworks,
+  validateChainId,
+  validateEthRpc,
+  validateSysRpc,
+} from '../src/rpc';
+import {
+  BLOCKBOOK_RPC_URL,
+  CHAIN_ID_HEX,
+  CHAIN_ID_NUMBER,
+  RPC_URL,
+  VALID_BIP44_DATA_RESPONSE,
+  VALID_BLOCKBOOK_RPC_RESPONSE,
+  VALID_ETH_RPC_RESPONSE,
+} from './constants';
+
+describe('rpc tests', () => {
+  it('should check if given chain id is a safe integer and it is between 0 and 4503599627370476', () => {
+    const isValid = isValidChainIdForEthNetworks(CHAIN_ID_NUMBER);
+
+    expect(isValid).toBe(true);
+  });
+
+  it('should validate an ethereum chain id', () => {
+    const isValid = validateChainId(CHAIN_ID_NUMBER);
+
+    expect(isValid.valid).toBe(true);
+    expect(isValid.hexChainId).toBe(CHAIN_ID_HEX);
+  });
+
+  it('should validate given eth rpc', async () => {
+    const response = await validateEthRpc(RPC_URL);
+
+    expect(response).toStrictEqual(VALID_ETH_RPC_RESPONSE);
+  });
+
+  it('should validate given trezor blockbook rpc', async () => {
+    const response = await validateSysRpc(BLOCKBOOK_RPC_URL);
+
+    expect(response).toStrictEqual(VALID_BLOCKBOOK_RPC_RESPONSE);
+  });
+
+  it('should get bip44 data for given coin', () => {
+    const isTestnet = VALID_BLOCKBOOK_RPC_RESPONSE.chain === 'test';
+
+    const response = getBip44Chain(
+      VALID_BLOCKBOOK_RPC_RESPONSE.coin,
+      isTestnet
+    );
+
+    expect(response).toStrictEqual(VALID_BIP44_DATA_RESPONSE);
+  });
+});
