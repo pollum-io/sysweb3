1814151cc725aa7957b303a612a20f13a8d1fac8
claudiovb <claudiocarvalhovilasboas@gmail.com>
2023-04-10 15:47:21 -0300

    define function on types

--- packages/sysweb3-keyring/src/types.ts
+++ packages/sysweb3-keyring/src/types.ts
@@ -165,6 +165,7 @@ export interface IKeyringManager {
   setStorage: (client: any) => void;
   ethereumTransaction: IEthereumTransactions;
   syscoinTransaction: ISyscoinTransactions;
+  verifyIfIsTestnet: () => boolean | undefined;
 }
 
 export enum KeyringAccountType {
